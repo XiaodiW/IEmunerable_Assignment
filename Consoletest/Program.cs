072d291649d30f2c39828be32e502b6de900ab31
Xiaodi Wang <xiaodi.wang@student.forsbergsskola.se>
2021-01-14 19:53:33 +0100

    Initial commit

---
+++ Consoletest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            /*IEnumerable array = new[] {1, 2, 3, 4};
+            foreach (var number in array) {
+                Console.WriteLine("Foreach: "+number);
+            }
+            var enumerator = array.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine("Enumerator: " + enumerator.Current);
+            }*/
+            
+            var enumerable = new AllNumbersLooping(3, 7);
+            enumerable.AlsoLoopTo(5);
+            enumerable.AlsoLoopTo(9);
+            var enumerator = enumerable.GetEnumerator();
+            while (enumerator.MoveNext()) Console.Write($"{enumerator.Current};");
+        }
+    }
+}
