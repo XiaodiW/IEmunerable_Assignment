0f0d4b167b1d196a88ed502507a7e5ea9373bb2f
Xiaodi Wang <xiaodi.wang@student.forsbergsskola.se>
2021-01-14 20:06:38 +0100

    Add AllNumbersLooping

---
+++ Consoletest/AllNumbersLooping.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+
+namespace HelloWorld
+{
+    public class AllNumbersLooping : IEnumerable
+    {
+        private readonly List<int> givenNumbers = new();
+
+        public AllNumbersLooping(int from, int to) //Constructor
+        {
+            givenNumbers.Add(from);
+            givenNumbers.Add(to);
+        }
+
+        public void AlsoLoopTo(int number) => givenNumbers.Add(number);
+
+        public IEnumerator GetEnumerator()
+        {
+            for (var i = 0; i < givenNumbers.Count - 1; i++)
+            {
+                var a = givenNumbers[i];
+                var b = givenNumbers[i + 1];
+                a = i == 0 ? a : a + (a < b ? 1 : -1);
+                if (a < b)
+                    for (var j = a; j < b + 1; j++)
+                        yield return j;
+                else
+                    for (var j = a; j > b - 1; j--)
+                        yield return j;
+            }
+        }
+    }
+}
